d8b9106b6be05baf654445324dd8f888d194e315
Joe Schafer <joeschaferastula@gmail.com>
2018-01-27 21:38:41 -0600

    adding really-smooth-scroll

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
 import { Background } from './App.style';
+import ReallySmoothScroll from 'really-smooth-scroll';
+
+ReallySmoothScroll.shim();
+
+ReallySmoothScroll.config({
+  mousewheelSensitivity: 6, // Default
+  keydownSensitivity: 6 // Default (When you press arrow down/up key)
+});
 
 class App extends Component {
   render() {
